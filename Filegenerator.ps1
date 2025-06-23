3e3c244ff686d59e0be392cf56d7a427853195b6
Chris Gardner <chris.gardner@section8games.co.uk>
2025-06-23 09:39:56 +0000

    Initial tests

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -8,33 +8,30 @@ $errorTypes = @(
     'Temperature warning'
 )
 $statusCodes = 'OK','WARN','ERR'
+
  
-$logLines = @()
- 
-for ($i=0; $i -lt 50000; $i++) {
+$Loglines= foreach ($i in (1..50000)) {
     $timestamp = (Get-Date).AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")
-    $plc = $plcNames | Get-Random
+    $plc = Get-Random -InputObject $plcNames
     $operator = Get-Random -Minimum 101 -Maximum 121
     $batch = Get-Random -Minimum 1000 -Maximum 1101
-    $status = $statusCodes | Get-Random
+    $status = Get-Random -InputObject $statusCodes
     $machineTemp = [math]::Round((Get-Random -Minimum 60 -Maximum 110) + (Get-Random),2)
     $load = Get-Random -Minimum 0 -Maximum 101
  
     if ((Get-Random -Minimum 1 -Maximum 8) -eq 4) {
-        $errorType = $errorTypes | Get-Random
+        $errorType = Get-Random -InputObject $errorTypes
         if ($errorType -eq 'Sandextrator overload') {
             $value = (Get-Random -Minimum 1 -Maximum 11)
-            $msg = "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
+            "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
         } else {
-            $msg = "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
+            "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
         }
     } else {
-        $msg = "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
+        "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
     }
- 
-    $logLines += $msg
 }
  
-Set-Content -Path $bigFileName -Value $logLines
+#set-Content -Path $bigFileName -Value $logLines
 Write-Output "PLC log file generated."
 }
